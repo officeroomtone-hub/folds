1f96804f1e284b5df74c34007d08383d3aeb6db4
leo <office.roomtone@gmail.com>
2026-06-27 16:39:43 +0200

    just a little cleanup of the codebase

--- paperfolds.c
+++ paperfolds.c
@@ -3,12 +3,9 @@
 #include <string.h>
 #include <stdlib.h>
 
-/* long long folds(int x, int n){
-   return x * pow(2, n);
-} */
-
 long long folds(int x, int n) {
-   return (long long)x << n; // Shifts the bits of x to the left by n places which results in the doubling of the value
+   return (long long)x << n; /* Shifts the bits of x to the left by n places which results in the doubling of the values
+   replacing the pow() function from the math.h library */
 }
 int main(int argc, char *argv[]){
 
@@ -23,7 +20,7 @@ int main(int argc, char *argv[]){
       return 1; // Safety check for EOF/error
   }
 
-  // input [strcspn(input, "\n")] = '\0';
+  input [strcspn(input, "\n")] = '\0';
 
   char *in_tokens = strtok(input, ", ");
     while (in_tokens != NULL && i < 2){
@@ -31,14 +28,12 @@ int main(int argc, char *argv[]){
       i++;
       in_tokens = strtok(NULL, ", ");
       };
- 
-    //for (int j = 0; j < i; j++){
-      //printf("%d\n", tokenarr[j]);
-     // }
 
   long long y = folds(tokenarr[0],tokenarr[1]);
   printf("you end up with %lld folds or grains of rice or shmoney if you start with %d and double it %d times\n", y, tokenarr[0], tokenarr[1]);
 
   double distance = y * 0.00012;
   printf("this is the equivalent of %.4f meters at a paper thickness of 0.12mm\n", distance);
+
+  return 0;
 }
